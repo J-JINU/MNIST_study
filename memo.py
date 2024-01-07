0e969e218bae459433ab4094ef70ed154039f687
JINWOOJay <woo73202@gmail.com>
2024-01-08 00:24:34 +0900

    start mnist study

---
+++ memo.py
@@ -0,0 +1,5 @@
+import cupy as cp
+
+A = cp.array([1, 2, 3])
+
+print(A)
---
+++ modules/activation_func.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cupy as cp
+import polars as po
+
+class activate:
+    def __init__(self) -> None:
+        pass
+    
+    
