3076a3c7a83eeeb61da184ee969103fd2182f96a
JINWOOJay <woo73202@gmail.com>
2024-01-10 01:06:53 +0900

    modify neuralnet.py and study import absolute path

--- modules/neuralnet.py
+++ modules/neuralnet.py
@@ -1,4 +1,7 @@
-# import numpy as np
+import sys, os
+import pickle
+
+import numpy as np
 cupy_enable = True
 try:
     import cupy as np
@@ -6,10 +9,31 @@ except ImportError:
     import numpy as np
     cupy_enable = False
 
+print('__file__ ::', __file__)
+print('os.path.dirname(__file__) ::', os.path.dirname(__file__))
+print('os.path.abspath(os.path.dirname(__file__)) ::', os.path.abspath(os.path.dirname(__file__)))
+print('os.path.dirname(os.path.abspath(os.path.dirname(__file__))) ::', os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+print('os.path.dirname(os.path.dirname(__file__)) ::', os.path.dirname(os.path.dirname(__file__)))
+from deep_study.dataset.mnist import load_mnist
+from function import *
+
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
 
 def init_network():
-    pass
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
 
-def forward(network, x):
-    pass
-
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) +_b3
+    return softmax(a3)
