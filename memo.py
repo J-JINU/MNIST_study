83a024d86569ade8f70915ee4c5b26007007beed
J-JINU <woo73202@gmail.com>
2024-01-10 21:46:09 +0900

    .

--- memo.py
+++ memo.py
@@ -1,6 +1,8 @@
 import time
 import polars as po
 import matplotlib.pyplot as plt
+from modules import neuralnet as net
+from PIL import Image
 
 cupy_enable = True
 try:
@@ -9,4 +11,30 @@ except ImportError:
     import numpy as np
     cupy_enable = False
 
-A = np.array([1, 2, 3])
+# def img_show(img):
+#     pil_img = Image.fromarray(np.uint8(img))
+#     pil_img.show()
+
+# x_train, t_train, x_test, t_test = net.get_data_all(normalize=False)
+
+# img = x_train[7]
+# label = t_train[7]
+# print(label)
+
+# print(img.shape)
+# img = img.reshape(28, 28)
+# print(img.shape)
+
+# img_show(img)
+
+x, t = net.get_data()
+network = net.init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = net.predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print("Accuracy : " + str(float(accuracy_cnt) / len(x)))
--- modules/neuralnet.py
+++ modules/neuralnet.py
@@ -9,11 +9,15 @@ except ImportError:
     import numpy as np
     cupy_enable = False
 
-print('__file__ ::', __file__)
-print('os.path.dirname(__file__) ::', os.path.dirname(__file__))
-print('os.path.abspath(os.path.dirname(__file__)) ::', os.path.abspath(os.path.dirname(__file__)))
-print('os.path.dirname(os.path.abspath(os.path.dirname(__file__))) ::', os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-print('os.path.dirname(os.path.dirname(__file__)) ::', os.path.dirname(os.path.dirname(__file__)))
+# print('__file__ ::', __file__)
+# print('os.path.dirname(__file__) ::', os.path.dirname(__file__))
+# print('os.path.abspath(os.path.dirname(__file__)) ::', os.path.abspath(os.path.dirname(__file__)))
+# print('os.path.dirname(os.path.abspath(os.path.dirname(__file__))) ::', os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+# print('os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))) ::', os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+# print('os.path.dirname(os.path.dirname(__file__)) ::', os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(__file__))
+
 from deep_study.dataset.mnist import load_mnist
 from function import *
 
@@ -21,6 +25,10 @@ from function import *
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
+
+def get_data_all(normalize=True, flatten=True, one_hot_label=False):
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=normalize, flatten=flatten, one_hot_label=one_hot_label)
+    return x_train, t_train, x_test, t_test
 
 def init_network():
     with open("sample_weight.pkl", 'rb') as f:
