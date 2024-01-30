ffd6a5876394603bcf51104b203f612e2731d7e4
JINWOOJay <woo73202@gmail.com>
2024-01-31 02:19:12 +0900

    need performance test 4 dimension matrix dot product(1.direct 4dim, 2.reshape 2dim)

--- memo.py
+++ memo.py
@@ -16,46 +16,62 @@ import cupy as np
 
 from deep_study.dataset.mnist import load_mnist
 
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-x_train = np.asarray(x_train)
-t_train = np.asarray(t_train)
-x_test = np.asarray(x_test)
-t_test = np.asarray(t_test)
+# arr = np.array([[[[1, 2], [3, 4]], [[5, 6], [7, 8]]], [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]])
+# print(arr.shape)
 
-train_loss_list = []
+arr = np.arange(0, 16)
+arr = arr.reshape(4, 1, 1, -1) # -1입력하면 자동으로 4로 계산되어 들어감.
+arr2 = arr.reshape(2, 1, 2, -1)
+print(arr2)
+print(arr2.shape)
+arr2_origin_shape = arr2.shape
+arr2 = arr2.reshape(arr2.shape[0], -1)
+print(arr2)
+print(arr2.shape)
+arr2 = arr2.reshape(*arr2_origin_shape)
+print(arr2)
+print(arr2.shape)
 
-iters_num = 10000
-train_size = x_train.shape[0]
-batch_size = 100
-learning_rate = 0.1
-network = nnet.TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+# (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+# x_train = np.asarray(x_train)
+# t_train = np.asarray(t_train)
+# x_test = np.asarray(x_test)
+# t_test = np.asarray(t_test)
 
-pred_start = time.perf_counter()
-for i in range(iters_num):
-    print(f'start {i} iter')
-    start_time = time.perf_counter()
-    batch_mask = np.random.choice(train_size, batch_size)
-    x_batch = x_train[batch_mask]
-    t_batch = t_train[batch_mask]
+# train_loss_list = []
+
+# iters_num = 10000
+# train_size = x_train.shape[0]
+# batch_size = 100
+# learning_rate = 0.1
+# network = nnet.TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+# pred_start = time.perf_counter()
+# for i in range(iters_num):
+#     print(f'start {i} iter')
+#     start_time = time.perf_counter()
+#     batch_mask = np.random.choice(train_size, batch_size)
+#     x_batch = x_train[batch_mask]
+#     t_batch = t_train[batch_mask]
     
-    grad = network.numerical_gradient(x_batch, t_batch)
+#     grad = network.numerical_gradient(x_batch, t_batch)
     
-    for key in ('W1', 'b1', 'W2', 'b2'):
-        network.params[key] -= learning_rate * grad[key]
+#     for key in ('W1', 'b1', 'W2', 'b2'):
+#         network.params[key] -= learning_rate * grad[key]
         
-    loss = network.loss(x_batch, t_batch)
-    train_loss_list.append(loss)
-    end_time = time.perf_counter()
-    print(f'end {i} iter, run time : {end_time - start_time}')
+#     loss = network.loss(x_batch, t_batch)
+#     train_loss_list.append(loss)
+#     end_time = time.perf_counter()
+#     print(f'end {i} iter, run time : {end_time - start_time}')
     
-pred_end = time.perf_counter()
-print(f'end pred, total time : {pred_end - pred_start}')
-# 그래프 그리기
-markers = {'train': 'o', 'test': 's'}
-x = np.arange(len(train_loss_list))
-plt.plot(x, train_loss_list, label='train loss')
-plt.xlabel("epochs")
-plt.ylabel("accuracy")
-plt.ylim(0, 1.0)
-plt.legend(loc='lower right')
-plt.show()
+# pred_end = time.perf_counter()
+# print(f'end pred, total time : {pred_end - pred_start}')
+# # 그래프 그리기
+# markers = {'train': 'o', 'test': 's'}
+# x = np.arange(len(train_loss_list))
+# plt.plot(x, train_loss_list, label='train loss')
+# plt.xlabel("epochs")
+# plt.ylabel("accuracy")
+# plt.ylim(0, 1.0)
+# plt.legend(loc='lower right')
+# plt.show()
--- modules/neuralnet.py
+++ modules/neuralnet.py
@@ -68,7 +68,7 @@ class simpleNet:
         loss = cross_entropy_error(y, t)
         
         return loss
-    
+
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         self.params = {}
@@ -106,8 +106,8 @@ class TwoLayerNet:
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
         
         return grads
-    
-    
+
+
 class MulLayer:
     def __init__(self):
         self.x = None
@@ -123,7 +123,7 @@ class MulLayer:
         dx = dout * self.y
         dy = dout * self.x
         return dx, dy
-    
+
 
 class AddLayer:
     def __init__(self):
@@ -137,3 +137,53 @@ class AddLayer:
         dx = dout * 1
         dy = dout * 1
         return dx, dy
+
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+        
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask]
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class sigmoid:
+    def __init__(self):
+        self.out = None
+        
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+        
+    def forwaard(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
