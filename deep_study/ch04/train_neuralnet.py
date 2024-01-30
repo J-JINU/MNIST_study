7a773a582c735b7f965c4b6e80de04c2db73bfcc
JINWOOJay <woo73202@gmail.com>
2024-01-31 00:42:50 +0900

    modify numpy -> cupy

--- deep_study/ch04/train_neuralnet.py
+++ deep_study/ch04/train_neuralnet.py
@@ -1,13 +1,18 @@
 # coding: utf-8
+import time
 import sys, os
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
-import numpy as np
+import cupy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
 # 데이터 읽기
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+x_train = np.asarray(x_train)
+t_train = np.asarray(t_train)
+x_test = np.asarray(x_test)
+t_test = np.asarray(t_test)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -24,6 +29,7 @@ test_acc_list = []
 # 1에폭당 반복 수
 iter_per_epoch = max(train_size / batch_size, 1)
 
+start_time = time.perf_counter()
 for i in range(iters_num):
     # 미니배치 획득
     batch_mask = np.random.choice(train_size, batch_size)
@@ -31,8 +37,8 @@ for i in range(iters_num):
     t_batch = t_train[batch_mask]
     
     # 기울기 계산
-    grad = network.numerical_gradient(x_batch, t_batch)
-    # grad = network.gradient(x_batch, t_batch)
+    # grad = network.numerical_gradient(x_batch, t_batch)
+    grad = network.gradient(x_batch, t_batch)
     
     # 매개변수 갱신
     for key in ('W1', 'b1', 'W2', 'b2'):
@@ -49,7 +55,8 @@ for i in range(iters_num):
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
         print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
-
+end_time = time.perf_counter()
+print(f"full time is {end_time - start_time} s")
 # 그래프 그리기
 markers = {'train': 'o', 'test': 's'}
 x = np.arange(len(train_acc_list))
